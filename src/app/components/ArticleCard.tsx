fca055680a89196dc6edfd22dbbd2b84317e30f1
prokoposgiannis <giannisprokopos@gmail.com>
2024-08-29 00:18:34 +0300

    basic implementation dane

--- src/app/components/ArticleCard.tsx
+++ src/app/components/ArticleCard.tsx
@@ -1,21 +1,37 @@
+import Image from 'next/image'
 import { FC } from "react";
 
 interface ArticleCardProps {
     content: {
-        year: number;
-        title: string;
-        id: number;
+        // anchor?: string;
+        // year: string;
+        // text: string;
+        // id: number;
+    anchor: string;
+    imageUrl: string;
+    year: string;
+    text: string;
+    id: number;
+    tags: string[];
     };
 }
 
 const ArticleCard: FC<ArticleCardProps> = ({ content }) => {
+    // console.log(content);
     return (
-        <div className="flex flex-col lg:flex-row h-auto lg:h-[40rem] w-full lg:w-[70rem] bg-customGray m-6 p-6 lg:p-10 justify-evenly shadow-custom rounded-custom">
+        <div className="flex flex-col lg:flex-row h-auto lg:h-[30rem] w-full lg:w-[60rem] bg-customGray m-6 p-6 lg:p-10 justify-evenly shadow-custom rounded-custom">
             <div className="w-full lg:w-1/2 h-60 lg:h-auto bg-slate-500 rounded-2xl mb-6 lg:mb-0">
+            <Image
+                src={`${content.imageUrl}`}
+                alt="External Image"
+                unoptimized
+                width={300}
+                height={300}
+            />
             </div>
             <div className="w-full lg:w-1/2 text-xl lg:text-2xl px-4 lg:px-6">
                 <h1 className="text-center mb-4">{content.year}</h1>
-                <p>{content.title}</p>
+                <p>{content.text}</p>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,15 @@
 import ArticleCard from "@/app/components/ArticleCard";
 import RotateOnScroll from "@/app/components/RotateOnScroll";
+import { cnt } from "@/app/firebase"
 
-const content = [{
-  year: 553,
-  title: "Αρχίζει στην Κωνσταντινούπολη η Ε΄ Αγία Οικουμενική Σύνοδος.",
-  id: 1
-},
-{
-  year: 1260,
-  title: "Ο Κουμπλάι Χαν γίνεται κυβερνήτης της Μογγολικής αυτοκρατορίας.",
-  id: 2
-},
-{
-  year: 1494,
-  title: "Ο Χριστόφορος Κολόμβος αποβιβάζεται στην Τζαμάικα και τη διεκδικεί για την Ισπανία.",
-  id: 3
-},
-{
-  year: 1762,
-  title: "Η Ρωσία και η Πρωσία υπογράφουν τη συνθήκη της Αγίας Πετρούπολης.",
-  id: 4
-}]
+export default async function Home() {
 
-export default function Home() {
   return (
     <main className="flex min-h-screen overflow-x-hidden">
       <div className="w-2/12 bg-sidePanelsColor">
       </div>
       <div className="flex overflow-x-hidden flex-col w-10/12 min-h-screen justify-center items-center bg-customGray" >
-        {content.map((c)=> <ArticleCard key={c.id} content={c}/>)}
+        {cnt.map((c)=> <ArticleCard key={c.id} content={c}/>)}
       </div>
       <div className=" w-2/12 bg-sidePanelsColor">   
       <RotateOnScroll position={"-top-24"}/>   
